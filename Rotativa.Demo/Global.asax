a9c674e16a18f1c4b6b567f924495b243cab8021
Alex Munteanu <alex.munteanu@cvu.ro>
2017-07-12 18:28:26 +0300

    Initial version of the .NET Core port.

--- Rotativa.Demo/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rotativa.Demo.MvcApplication" Language="C#" %>
---
+++ Rotativa.NetCore.Demo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rotativa.NetCore.Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return new ActionAsPdf("Index");
+        }
+    }
+}
